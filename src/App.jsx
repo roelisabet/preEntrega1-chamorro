85284f72451e2679114b9feb277935cfa981be2c
roelisabet <roelisabet5@gmail.com>
2024-03-17 19:11:08 -0300

    haiendo cambios

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <div>
 
     <NavBar/>
-    <ItemListContainer greeting= "Bienvenidos a mi tienda" />
+    <ItemListContainer greeting= "Bienvenidos a mi tiendaOnline" />
 
     </div>
     </>
